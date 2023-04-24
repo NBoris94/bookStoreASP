73b38a46df250237a0e66d77590d6968c4506868
NBoris1994 <nboris1994@yandex.ru>
2023-04-24 11:26:25 +0300

    Сделал авторизацию на основе JWT

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -35,5 +35,10 @@ namespace BookStore.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
     }
 }
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Repository;
 using Repository.AuthorRepo;
 using Repository.BookRepo;
@@ -6,6 +9,15 @@ using Repository.GenreRepo;
 using Service.AuthorSer;
 using Service.BookSer;
 using Service.GenreSer;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+var users = new List<User>
+{
+    new User("nboris@mail.ru", "12345"),
+    new User("test@mail.ru", "55555")
+};
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +36,22 @@ builder.Services.AddTransient<IGenreService, GenreService>();
 builder.Services.AddTransient<IAuthorService, AuthorService>();
 builder.Services.AddTransient<IBookService, BookService>();
 
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidateAudience =  true,
+            ValidAudience = AuthOptions.AUDIENCE,
+            ValidateLifetime = true,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true
+        };
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,10 +67,52 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapPost("/login", (User loginData) =>
+{
+    var user = users.FirstOrDefault(u => u.Email == loginData.Email && u.Password == loginData.Password);
+
+    if (user == null)
+    {
+        return Results.Unauthorized();
+    }
+
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.Email) };
+
+    var jwt = new JwtSecurityToken(
+        issuer: AuthOptions.ISSUER,
+        audience: AuthOptions.AUDIENCE,
+        claims: claims,
+        expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+        signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+    var response = new
+    {
+        access_token = encodedJwt,
+        username = user.Email
+    };
+
+    return Results.Json(response);
+});
+
+app.Map("/data", [Authorize] (HttpContext context) => $"ַהנאסüעו");
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
+public class AuthOptions
+{
+    public const string ISSUER = "AuthServer";
+    public const string AUDIENCE = "AuthClient";
+    const string KEY = "mysupersecret_key_1Q2w3e4R_";
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+}
+
+record class User(string Email, string Password);
